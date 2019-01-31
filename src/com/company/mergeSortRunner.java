1c2ad69e3432a05e4fed44607a5a2e2334a3e890
BT_1N3_02@bktech.local <BT_1N3_02@bktech.local>
2019-01-31 08:38:27 -0500

    Initial commit

---
+++ src/com/company/mergeSortRunner.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class mergeSortRunner {
+
+    public static void main(String[] args)
+    {
+	// write your code here
+
+        int arr[] = {4, 6, 2, 7};
+        int arr[] = {3, 9, 8, 5};
+    }
+}
